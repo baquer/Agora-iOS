a7fd3a91c49e39e9f829763535e04f4873454a21
baquer <baquerali@icloud.com>
2020-02-26 00:57:13 +0530

    Implemented SignUp ViewController and drop down text field

--- Agora_iOS/Agora_iOS/Controllers/SignUpViewController.swift
+++ Agora_iOS/Agora_iOS/Controllers/SignUpViewController.swift
@@ -7,25 +7,52 @@
 //
 
 import UIKit
+import Material
+import IQDropDownTextField
 
 class SignUpViewController: UIViewController {
 
+    
+    @IBOutlet weak var userNameTextField: TextField!
+    
+    @IBOutlet weak var firstNameTextField: TextField!
+    
+    @IBOutlet weak var lastNameTextField: TextField!
+    
+    @IBOutlet weak var emailTextField: TextField!
+    
+    @IBOutlet weak var passwordTextField: TextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var quesDropDownTextField: IQDropDownTextField!
+
+    @IBOutlet weak var ansTextField: TextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setUpAttributes()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpAttributes() {
+        signUpButton.layer.cornerRadius = 20.0
+        passwordTextField.isVisibilityIconButtonEnabled = true
+        passwordTextField.dividerNormalColor = UIColor.systemOrange
+        passwordTextField.dividerActiveColor = UIColor.systemOrange
+        userNameTextField.dividerNormalColor = UIColor.systemOrange
+        userNameTextField.dividerActiveColor = UIColor.systemOrange
+        firstNameTextField.dividerNormalColor = UIColor.systemOrange
+        firstNameTextField.dividerActiveColor = UIColor.systemOrange
+        lastNameTextField.dividerNormalColor = UIColor.systemOrange
+        lastNameTextField.dividerActiveColor = UIColor.systemOrange
+        emailTextField.dividerNormalColor = UIColor.systemOrange
+        emailTextField.dividerActiveColor = UIColor.systemOrange
+        ansTextField.dividerNormalColor = UIColor.systemOrange
+        ansTextField.dividerActiveColor = UIColor.systemOrange
+        quesDropDownTextField.isOptionalDropDown = false
+        quesDropDownTextField.itemList = [Constants.securityQuestions.quesOne, Constants.securityQuestions.quesTwo, Constants.securityQuestions.quesThree, Constants.securityQuestions.quesFour, Constants.securityQuestions.quesFive]
+        Utilities.styleTextField(quesDropDownTextField)
     }
-    */
-    
 
 }
---
+++ Agora_iOS/Agora_iOS/Helpers/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Agora_iOS
+//
+//  Created by Syed on 26/02/20.
+//  Copyright © 2020 Syed. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Constants {
+    struct securityQuestions {
+        static let quesOne = "What is your Mother's maiden name?"
+        static let quesTwo = "What was your childhood nickname?"
+        static let quesThree = "What is the name of your favourite childhood friend?"
+        static let quesFour = "In what city or town was your first job?"
+        static let quesFive = "What was the last name of the third grade teacher?"
+    }
+}
